6fd4e37275d711233e0928a34a5f918931f2938a
Odds <panerjeffryjames@gmail.com>
2023-10-07 18:50:17 +0800

    fix display client app table

--- admin/assets/js/rememberInput.js
+++ admin/assets/js/rememberInput.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    // 1. Load values from local storage on page load
-    const formInputs = document.querySelectorAll('#application_form input, #application_form select');
-    formInputs.forEach(input => {
-        const storedValue = localStorage.getItem(input.name);
-        if (storedValue) {
-            input.value = storedValue;
-        }
-    });
+//     // 1. Load values from local storage on page load
+//     const formInputs = document.querySelectorAll('#application_form input, #application_form select');
+//     formInputs.forEach(input => {
+//         const storedValue = localStorage.getItem(input.name);
+//         if (storedValue) {
+//             input.value = storedValue;
+//         }
+//     });
 
-    // 2. Store values in local storage when they change
-    formInputs.forEach(input => {
-        input.addEventListener('input', function () {
-            localStorage.setItem(input.name, input.value);
-        });
-    });
+//     // 2. Store values in local storage when they change
+//     formInputs.forEach(input => {
+//         input.addEventListener('input', function () {
+//             localStorage.setItem(input.name, input.value);
+//         });
+//     });
 
-    // 3. Clear local storage data after submitting
-    document.querySelector("#application_form").addEventListener('submit', function () {
-        formInputs.forEach(input => {
-            localStorage.removeItem(input.name);
-        });
-    });
-});
+//     // 3. Clear local storage data after submitting
+//     document.querySelector("#application_form").addEventListener('submit', function () {
+//         formInputs.forEach(input => {
+//             localStorage.removeItem(input.name);
+//         });
+//     });
+// });
