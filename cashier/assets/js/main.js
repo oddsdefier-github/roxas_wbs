6e449dda08769fa786c159426a883c5ffb92a4b7
Odds <panerjeffryjames@gmail.com>
2023-10-25 23:58:56 +0800

    last commit

--- cashier/assets/js/main.js
+++ cashier/assets/js/main.js
@@ -3,7 +3,6 @@ import { DataTableWithPagination } from './DataTableWithPagination.js';
 $(document).ready(function () {
     const currentPath = window.location.pathname;
     const filename = currentPath.substring(currentPath.lastIndexOf('/') + 1);
-    console.log(`Current Filename: ${filename}`); // Debugging line
 
     if (filename === 'billing_payments.php') {
         const billingTable = new DataTableWithPagination("billing_data", '#displayBillingTable');
@@ -15,11 +14,9 @@ $(document).ready(function () {
         const linkHref = $(this).attr('href');
         const linkFilename = linkHref.substring(linkHref.lastIndexOf('/') + 1);
 
-        console.log(`Link Filename: ${linkFilename}`); // Debugging line
-
         if (linkFilename === filename) {
-            console.log('Match Found: Applying Styles'); // Debugging line
             $(this).removeClass('inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50').addClass('inline-block p-4 text-primary-600 bg-gray-100 rounded-t-lg active');
         }
     });
 });
+
